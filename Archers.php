2f0e484950c9b5cfca4b7bbf4af8f43aa9ec4fdf
WillTonks <166097801+WillTonks@users.noreply.github.com>
2025-05-28 16:05:23 +1000

    Add files via upload

---
+++ Archers.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+ <!--
+filename: Archers.php
+author: Will Tonks
+created: 20/5/2025
+last modified: 20/5/2025
+description: Home page for Archers
+Tuesday 8:30am Class
+Tutor: Jenny Lai
+-->
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="Home Page for Archery Database Website" />
+    <meta name="keywords" content="HTML, CSS" />
+    <meta name="author" content="Will"  />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Archery Australia: Archers Homepage</title>
+    <link href= "style.css" rel="stylesheet"/>
+</head>
+<?php include 'header-Archer.inc';?>
+<body>
+    <h1>Archer's Interface</h1>
+    
+    
---
+++ settings.php
@@ -0,0 +1,6 @@
+<?php
+	$host = "feenix-mariadb.swin.edu.au";
+	$user = "s104468000";
+	$pwd = "090703";
+	$sql_db = "s104468000_db";
+?>
